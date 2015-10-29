a08665c61fc0a589f490e95816b46283b1043a1e
starandtina <kandmforever@126.com>
2015-10-29 14:24:18 +0800

    update jS/CSS to minified files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
-  $('.full-page').fullpage({
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', 'lastPage'],
-    navigation: true,
-    navigationPosition: 'right',
-    menu: '#menu',
-    css3: true,
-    scrollingSpeed: 1000
-  });
+    $('.full-page').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff', '#7BAABE'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', 'lastPage'],
+        navigation: true,
+        navigationPosition: 'right',
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 700,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom'
+    });
+
+    // popup images
+    $('.image-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        image: {
+            verticalFit: true
+        }
+    });
+
+    // popup videos
+    $('.popup-youku').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
 });
